1b7f40fe3818167539ae3e6bebd6ad52be960309
Natalia Hess <nhess@natalia-hesss-macbook-pro.local>
2016-09-20 13:40:11 -0700

    updates routes

--- starter-code/public/app.js
+++ starter-code/public/app.js
@@ -2,3 +2,12 @@ console.log('app.js connected');
 $(document).ready(function(){
   console.log('DOM ready');
 });
+
+$.ajax({
+  type: 'GET',
+  url: 'http://localhost:3000/pickanumber',
+  dataType: 'json',
+  success: function(data) {
+    console.log("Request successful");
+  }
+});
--- starter-code/server.js
+++ starter-code/server.js
@@ -22,13 +22,44 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // ROUTES
 // Root Route
+/* Create a root route that responds by sending the index.html file from the views directory. */
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+
 
 
 // Gallery View Route
 
 
 // The Number Guessing Game
+/* Build a route that allows a user to guess a number through query
+parameters (for example, /pickanumber?number=10).
+The route should check the user's number against some target number variable
+ stored in the server code. When the user navigates to this route in the browser,
+ the server should respond with either "Too High", "Too Low" or "Nailed it!"
+ For example, if the target number is 7, then a request to /pickanumber?number=10
+ should trigger a response of 'Too High'.*/
+app.get("/pickanumber", function (request, response) {
+  var number = +request.query.number;
+  var targetNum = 8;
+  if (number == targetNum) {
+    response.send("Nailed it!");
+  } else if (number < targetNum) {
+    response.send("Too low...");
+  } else if (number > targetNum) {
+    response.send("Too high...");
+  } else {
+    response.send("Hmm...not sure that's a number. Try again, goober.");
+  }
+});
 
+// pickanumber#Create
+app.post("/pickanumber", function (request, response) {
+  var newTargetNum = +request.body.number;
+  response.json(newTargetNum);
+});
 
 // Gallery
 
